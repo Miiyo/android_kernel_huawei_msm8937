6ac66a9a1ffd662d1300d5591e37afa75e9cc1d1
Archana Ramachandran <archanar@codeaurora.org>
2023-01-23 01:57:38 +0000

    prima: Fix NULL pointer dereferencing in p2p TX MGMT
    Fix dereferencing channel struct pointer without verifying the
    validity.

--- drivers/staging/prima/CORE/HDD/src/wlan_hdd_p2p.c
+++ drivers/staging/prima/CORE/HDD/src/wlan_hdd_p2p.c
@@ -1445,7 +1445,9 @@ int __wlan_hdd_mgmt_tx( struct wiphy *wiphy, struct net_device *dev,
     }
     //If GO adapter exists and operating on same frequency
     //then we will not request remain on channel
-    if (ieee80211_frequency_to_channel(chan->center_freq) == home_ch)
+    if (chan &&
+        (ieee80211_frequency_to_channel(chan->center_freq) ==
+        home_ch))
     {
         /*  if GO exist and is not off channel
          *  wait time should be zero.
